15d41e1d27dc4186ee16fc9886996b19f5c43993
Anu0408 <ac298@njit.edu>
2023-04-14 14:41:41 -0400

    updated .env

--- BusinessManagement/views/admin.py
+++ BusinessManagement/views/admin.py
@@ -1,9 +1,10 @@
 import io
-import csv
 from flask import Blueprint, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 from sql.db import DB
 import traceback
+from collections import Counter
+import csv
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
 @admin.route("/import", methods=["GET","POST"])
@@ -14,18 +15,15 @@ def importCSV():
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        print('file name1 --',file.filename, file.filename.lower().endswith('.csv'))
         if file.filename == '':
             flash('No selected file', "warning")
             return redirect(request.url)
-         # TODO importcsv-1 check that it's a .csv file, return a proper flash message if it's not
-        elif file.filename.lower().endswith('.csv'):
-            print('file name --',file.filename)
-        else:
-            flash('Invalid File ', "warning")
+        #UCID: ac298
+        # TODO importcsv-1 check that it's a .csv file, return a proper flash message if it's not
+        
+        if file.filename.split('.')[-1] != 'csv':
+            flash('Uploaded file is not CSV file', 'danger')
             return redirect(request.url)
-
-        #UCID:ac298; Date: 04/08/23
         if file and secure_filename(file.filename):
             companies = []
             employees = []
@@ -50,54 +48,48 @@ def importCSV():
                         company_id = (SELECT id FROM IS601_MP3_Companies WHERE name=%(company_name)s LIMIT 1)
             """
             # Note: this reads the file as a stream instead of requiring us to save it
+               #UCID: ac298
             stream = io.TextIOWrapper(file.stream._file, "UTF8", newline=None)
-            # TODO importcsv-2 read the csv file stream as a dict
-          
-            
             for row in csv.DictReader(stream, delimiter=','):
-                print(row) #example
-                # TODO importcsv-3 extract company data and append to company list 
-                # as a dict only with company data if all is present
-                if row["company_name"] and row["address"] and row["city"] and row["state"] and row["zip"] and row["web"] and row["country"]:
-                   companies.append({"name" : row["company_name"],  "address" :  row["address"], "city" : row["city"], "state" : row["state"], "country" : row["country"],
-                    "zip": row["zip"], "website" : row["web"]})
-                if row["first_name"] and row["last_name"] and row["email"] and row["company_name"]:
-                    employees.append({"first_name" : row["first_name"],  "last_name" :  row["last_name"], "email" : row["email"], "company_name" : row["company_name"]})
-                
-                # TODO importcsv-4 extract employee data and append to employee list 
-                # as a dict only with employee data if all is present
-            #UCID:ac298; Date: 04/08/23  
-               
+                # print(row) #example
+                # TODO importcsv-3 extract company data and append to company list as a dict only with company data
+                if (row["company_name"].strip() != "" and row["address"].strip() != "" and row["city"].strip() != "" and row["country"].strip() != "" and row["state"].strip() != "" and row["zip"].strip() != "" and row["web"].strip() != ""):
+                    companies.append({'name':row["company_name"], 'address':row["address"], 'city':row["city"], 'country':row["country"], 'state':row["state"], 'zip':row["zip"],
+                 'website':row["web"]})
+                    
+                    #UCID: ac298
+                # TODO importcsv-4 extract employee data and append to employee list as a dict only with employee data
+                if (row["first_name"].strip() != "" and row["last_name"].strip() != "" and row['email'].strip() != "" and row['company_name'].strip() != ""):
+                    employees.append({'first_name':row["first_name"], 'last_name':row["last_name"], 'email':row['email'], 'company_name':row['company_name']})
+
             if len(companies) > 0:
                 print(f"Inserting or updating {len(companies)} companies")
                 try:
                     result = DB.insertMany(company_query, companies)
                     # TODO importcsv-5 display flash message about number of companies inserted
+                    flash(f'{len(companies)} companies have been inserted', 'success')
                 except Exception as e:
                     traceback.print_exc()
                     flash("There was an error loading in the csv data", "danger")
             else:
                 # TODO importcsv-6 display flash message (info) that no companies were loaded
-                pass
+                # pass
+                   #UCID: ac298
+                flash("No companies are loaded")
             if len(employees) > 0:
                 print(f"Inserting or updating {len(employees)} employees")
                 try:
                     result = DB.insertMany(employee_query, employees)
                     # TODO importcsv-7 display flash message about number of employees loaded
-                    flash(f"Inserted {len(employees)} employees records successfully!", "success")
+                       #UCID: ac298
+                    flash(f'{len(employees)} employees have been inserted', "success")
                 except Exception as e:
                     traceback.print_exc()
                     flash("There was an error loading in the csv data", "danger")
-                    #UCID:ac298; Date: 04/08/23
             else:
-                 # TODO importcsv-8 display flash message (info) that no employees were loaded
-                  flash("No employees records were loaded", "danger")
-            return render_template("upload.html")
-            try:
-                result = DB.selectOne("SHOW SESSION STATUS LIKE 'questions'")
-                print(f"Result {result}")
-            except Exception as e:
-                    traceback.print_exc()
-                    flash("There was an error counting session queries", "danger")
+                # TODO importcsv-8 display flash message (info) that no companies were loaded
+                   #UCID: ac298
+                
+
+                flash("No employees are loaded")
     return render_template("upload.html")
-    #UCID:ac298; Date: 04/08/23
--- BusinessManagement/views/employee.py
+++ BusinessManagement/views/employee.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, flash, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from sql.db import DB
 employee = Blueprint('employee', __name__, url_prefix='/employee')
 
@@ -8,54 +8,51 @@ def search():
     rows = []
     # DO NOT DELETE PROVIDED COMMENTS
     # TODO search-1 retrieve employee id as id, first_name, last_name, email, company_id, company_name using a LEFT JOIN
-    query = """SELECT e.id, e.first_name, e.last_name, e.email, e.company_id, IF(c.name is not null, c.name,'N/A') AS company_name from 
-    IS601_MP3_Employees e LEFT JOIN IS601_MP3_Companies c ON e.company_id = c.id WHERE 1=1"""  
-    #UCID:ac298; DATE:04/08/23 
-    args = []  # <--- add values to replace %s/%(named)s placeholders
+    # Select query to get the required data
+    query = """SELECT e.id as id, e.first_name, e.last_name, e.email, e.company_id, c.name as company_name 
+    FROM IS601_MP3_Employees e LEFT JOIN IS601_MP3_Companies c ON e.company_id = c.id WHERE 1=1"""
+    args = [] # <--- append values to replace %s placeholders
     allowed_columns = ["first_name", "last_name", "email", "company_name"]
-    allowed_list = [(v, v) for v in allowed_columns]
+    allowed_list = [(v,v) for v in allowed_columns]
     # TODO search-2 get fn, ln, email, company, column, order, limit from request args
-    first_name = request.args.get('fn')
-    last_name = request.args.get('ln')
+    # retreving the arguments from form
+    fn = request.args.get('first_name')
+    ln = request.args.get('last_name')
     email = request.args.get('email')
     company = request.args.get('company')
-    limit = 10
     column = request.args.get('column')
     order = request.args.get('order')
     limit = request.args.get('limit')
-    #UCID:ac298; DATE:04/08/23 
     # TODO search-3 append like filter for first_name if provided
-    if first_name:
-        query += " AND e.first_name like %s"
-        args.append(f"%{first_name}%")
     # TODO search-4 append like filter for last_name if provided
-    if last_name:
-        query += " AND e.last_name like %s"
-        args.append(f"%{last_name}%")
     # TODO search-5 append like filter for email if provided
-    if email:
-        query += " AND email like %s"
-        args.append(f"%{email}%")
     # TODO search-6 append equality filter for company_id if provided
-    if company:
-        query += f" AND company_id = {company}"
-    #UCID:ac298; DATE:04/08/23 
     # TODO search-7 append sorting if column and order are provided and within the allowed columns and order options (asc, desc)
-    if column and order:
-        print(column, order)
-        if column in allowed_columns \
-            and order in ["asc", "desc"]:
-            query += f" ORDER BY {column} {order}"
     # TODO search-8 append limit (default 10) or limit greater than 1 and less than or equal to 100
+    # TODO search-9 provide a proper error message if limit isn't a number or if it's out of bounds
+    # adding the where statemnts based on input
+    if fn:
+        query += " AND e.first_name like %s"
+        args.append(f"%{fn}%")
+    if ln:
+        query += " AND e.last_name like %s"
+        args.append(f"%{ln}%")
+    if email:
+        query += " AND e.email like %s"
+        args.append(f"%{email}%")
+    if company:
+        query += " AND e.company_id = %s"
+        args.append(int(company))
+    if column and order:
+        if column in allowed_columns and order in ["asc", "desc"]:
+            query += f" ORDER BY {column} {order}"
     if limit and int(limit) > 0 and int(limit) <= 100:
         query += " LIMIT %s"
         args.append(int(limit))
-    # TODO search-9 provide a proper error message if limit isn't a number or if it's out of bounds
     elif limit and (int(limit) <= 0 or int(limit) > 100):
         flash("Enter the limit between 1 and 100", 'warning')
-    #UCID:ac298; DATE:04/08/23 
- 
-    print("query", query)
+    #UCID:ac298
+    print("query",query)
     print("args", args)
     try:
         result = DB.selectAll(query, *args)
@@ -63,16 +60,13 @@ def search():
             rows = result.rows
     except Exception as e:
         # TODO search-10 make message user friendly
-        flash(e, "error")
+        # flash message for exception
+        print(e)
+        flash("Error occured, please check the data and try again", "error")
     # hint: use allowed_columns in template to generate sort dropdown
-    # hint2: convert allowed_columns into a list of tuples representing (value, label)
-    # do this prior to passing to render_template, but not before otherwise it can break validation
-    
-
     return render_template("list_employees.html", rows=rows, allowed_columns=allowed_list)
-    
- 
-@employee.route("/add", methods=["GET", "POST"])
+
+@employee.route("/add", methods=["GET","POST"])
 def add():
     if request.method == "POST":
         # TODO add-1 retrieve form data for first_name, last_name, company, email
@@ -80,39 +74,38 @@ def add():
         # TODO add-3 last_name is required (flash proper error message)
         # TODO add-4 company (may be None)
         # TODO add-5 email is required (flash proper error message)
-        # TODO add-5a verify email is in the correct format
-        has_error = False  # use this to control whether or not an insert occurs
-
-        first_name = str(request.form.get('first_name'))
-        if not first_name:
-            has_error = True
-            flash("First name is required", "danger")
-        last_name = str(request.form.get('last_name'))
-        if not last_name:
-            has_error = True
-            flash("Last name is required", "danger")
-        email = str(request.form.get('email'))
-        if not email:
-            has_error = True
-            flash("email is required", "danger")
-        company = str(request.form.get('company')) or None
-    
-
-        if not has_error:
-            try:
-                result = DB.insertOne("""
-                INSERT INTO IS601_MP3_Employees
-                (first_name, last_name, company_id, email) VALUES (%s, %s, %s, %s);
-                """, first_name, last_name, company, email)  # <-- TODO add-6 add query and add arguments
-                if result.status:
-                    flash("Created Employee Record", "success")
-            except Exception as e:
-                print(e)
-                # TODO add-7 make message user friendly
-                flash(str(e), "danger")
+        # retrieving the employee data and checking for the required files
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        company = request.form.get("company")
+        print(company)
+        email = request.form.get("email")
+        if first_name == "":
+            flash("Enter first name", 'warning')
+            return redirect(request.url)
+        if last_name == "":
+            flash("Enter last name", 'warning')
+            return redirect(request.url)
+        if email == "":
+            flash("Enter email", 'warning')
+            return redirect(request.url)
+        #UCID: ac298
+        
+        try:
+            # Insert query to add the employee data to DB
+            result = DB.insertOne("""INSERT INTO IS601_MP3_Employees (first_name, last_name, email, company_id)
+                        VALUES (%(first_name)s, %(last_name)s, %(email)s, %(company_name)s)
+                        ON DUPLICATE KEY UPDATE first_name=%(first_name)s, last_name = %(last_name)s, email = %(email)s, company_id = %(company_name)s""",
+            {'first_name':first_name, 'last_name':last_name, 'email':email, 'company_name':company})
+            # <-- TODO add-6 add query and add arguments
+            if result.status:
+                flash("Created Employee Record", "success")
+        except Exception as e:
+            # TODO add-7 make message user friendly
+            # message for exception
+            print(e)
+            flash("Exception occured: " + str(e), "danger")
     return render_template("add_employee.html")
-    
-
 
 @employee.route("/edit", methods=["GET", "POST"])
 def edit():
@@ -127,26 +120,32 @@ def edit():
             last_name = str(request.form.get('last_name'))
             email = str(request.form.get('email'))
             company_id = str(request.form.get('company'))
+            #UCID: ac298
             # TODO edit-2 first_name is required (flash proper error message)
             if first_name == '' or first_name == None:
                 flash("first name is required", "danger")
                 return redirect("add")
+             #UCID: ac298
             # TODO edit-3 last_name is required (flash proper error message)
             if last_name == '' or last_name == None:
                 flash("last name is required", "danger")
                 return redirect("add")
+             #UCID: ac298
             # TODO edit-4 company (may be None)
             if company_id == '':
                 company_id = None
+                 #UCID: ac298
             # TODO edit-5 email is required (flash proper error message)
             if email == '' or email == None:
                 flash("email is required", "danger")
                 return redirect("add")
+             #UCID: ac298
             # TODO edit-5a verify email is in the correct format
             has_error = False  # use this to control whether or not an insert occurs
             
             if not has_error:
                 try:
+                     #UCID: ac298
                     # TODO edit-6 fill in proper update query
                     result = DB.update("""
                     UPDATE IS601_MP3_Employees SET first_name = %s, last_name = %s, company_id = %s, email = %s WHERE id = %s
@@ -155,11 +154,13 @@ def edit():
                     if result.status:
                         flash("Updated record", "success")
                 except Exception as e:
+                     #UCID: ac298
                     # TODO edit-7 make this user-friendly
                     flash(f" Following exception occured while updating the employee", "danger")
                     
         row = {}
         try:
+             #UCID: ac298
             # TODO edit-8 fetch the updated data
             result = DB.selectOne("""SELECT e.first_name, e.last_name, e.email, e.company_id, IF(c.name is not null, c.name,'N/A') AS 
             company_name from IS601_MP3_Employees e LEFT JOIN IS601_MP3_Companies c ON e.company_id = c.id WHERE e.id = %s""", id)
@@ -167,32 +168,28 @@ def edit():
             if result.status:
                 row = result.row
         except Exception as e:
+             #UCID: ac298
             # TODO edit-9 make this user-friendly
-            flash(f" Following exception occured while fetching the updated employee record", "danger")
+            flash(f"{e}", "danger")
+             #UCID: ac298
     # TODO edit-10 pass the employee data to the render template
     return render_template("edit_employee.html", employee=row)
-    
-
+ #UCID: ac298
 
 @employee.route("/delete", methods=["GET"])
 def delete():
+     #UCID: ac298
+
+    # code for deleting employee
+    id = request.args.get('id')
+    args = {**request.args}
     # TODO delete-1 delete employee by id
     # TODO delete-2 redirect to employee search
     # TODO delete-3 pass all argument except id to this route
     # TODO delete-4 ensure a flash message shows for successful delete
-    # TODO delete-5 if id is missing, flash necessary message and redirect to search
-    id = request.args.get("id")
-    
-    args = {**request.args}
     if id:
-        try:
-            result = DB.delete("DELETE FROM IS601_MP3_Employees WHERE id = %s", id)
-            if result.status:
-                flash("Deleted Employee record", "success")
-        except Exception as e:
-            flash(f" Following exception occured while deleting the employee record", "danger")
-        del args["id"]
-    flash(f" id is missing", "danger")
+        result = DB.delete("DELETE FROM IS601_MP3_Employees WHERE id = %s", id)
+        del args['id']
+        if result:
+            flash("Deleted successfully", 'success')
     return redirect(url_for("employee.search", **args))
-    pass
-    
