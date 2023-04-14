7e82aba3b2cc310bfc5bf279e03b52bd1ca2d19d
Anu0408 <ac298@njit.edu>
2023-04-14 19:18:10 -0400

    updated files

--- BusinessManagement/views/admin.py
+++ BusinessManagement/views/admin.py
@@ -3,7 +3,6 @@ from flask import Blueprint, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 from sql.db import DB
 import traceback
-from collections import Counter
 import csv
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -18,9 +17,8 @@ def importCSV():
         if file.filename == '':
             flash('No selected file', "warning")
             return redirect(request.url)
-        #UCID: ac298
+        # UCID: ac298
         # TODO importcsv-1 check that it's a .csv file, return a proper flash message if it's not
-        
         if file.filename.split('.')[-1] != 'csv':
             flash('Uploaded file is not CSV file', 'danger')
             return redirect(request.url)
@@ -48,48 +46,57 @@ def importCSV():
                         company_id = (SELECT id FROM IS601_MP3_Companies WHERE name=%(company_name)s LIMIT 1)
             """
             # Note: this reads the file as a stream instead of requiring us to save it
-               #UCID: ac298
             stream = io.TextIOWrapper(file.stream._file, "UTF8", newline=None)
+            # UCID: ac298
+            # TODO importcsv-2 read the csv file stream as a dict
             for row in csv.DictReader(stream, delimiter=','):
+                # pass # todo remove
                 # print(row) #example
-                # TODO importcsv-3 extract company data and append to company list as a dict only with company data
+                # UCID: ac298
+                # TODO importcsv-3 extract company data and append to company list 
+                # as a dict only with company data if all is present
                 if (row["company_name"].strip() != "" and row["address"].strip() != "" and row["city"].strip() != "" and row["country"].strip() != "" and row["state"].strip() != "" and row["zip"].strip() != "" and row["web"].strip() != ""):
                     companies.append({'name':row["company_name"], 'address':row["address"], 'city':row["city"], 'country':row["country"], 'state':row["state"], 'zip':row["zip"],
                  'website':row["web"]})
-                    
-                    #UCID: ac298
-                # TODO importcsv-4 extract employee data and append to employee list as a dict only with employee data
+                # UCID: ac298
+                # TODO importcsv-4 extract employee data and append to employee list 
+                # as a dict only with employee data if all is present
                 if (row["first_name"].strip() != "" and row["last_name"].strip() != "" and row['email'].strip() != "" and row['company_name'].strip() != ""):
                     employees.append({'first_name':row["first_name"], 'last_name':row["last_name"], 'email':row['email'], 'company_name':row['company_name']})
-
             if len(companies) > 0:
                 print(f"Inserting or updating {len(companies)} companies")
                 try:
                     result = DB.insertMany(company_query, companies)
+                    # UCID: ac298
                     # TODO importcsv-5 display flash message about number of companies inserted
                     flash(f'{len(companies)} companies have been inserted', 'success')
                 except Exception as e:
                     traceback.print_exc()
                     flash("There was an error loading in the csv data", "danger")
             else:
+                # UCID: ac298
                 # TODO importcsv-6 display flash message (info) that no companies were loaded
                 # pass
-                   #UCID: ac298
                 flash("No companies are loaded")
             if len(employees) > 0:
                 print(f"Inserting or updating {len(employees)} employees")
                 try:
                     result = DB.insertMany(employee_query, employees)
+                    # UCID: ac298
                     # TODO importcsv-7 display flash message about number of employees loaded
-                       #UCID: ac298
                     flash(f'{len(employees)} employees have been inserted', "success")
                 except Exception as e:
                     traceback.print_exc()
                     flash("There was an error loading in the csv data", "danger")
             else:
-                # TODO importcsv-8 display flash message (info) that no companies were loaded
-                   #UCID: ac298
-                
-
+                # UCID: ac298
+                # TODO importcsv-8 display flash message (info) that no employees were loaded
                 flash("No employees are loaded")
+                # pass
+            try:
+                result = DB.selectOne("SHOW SESSION STATUS LIKE 'questions'")
+                print(f"Result {result}")
+            except Exception as e:
+                    traceback.print_exc()
+                    flash(f"There was an error counting session queries", "danger")
     return render_template("upload.html")
